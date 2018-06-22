bdc675d6b3aeecaf1a8c2a724846eabd152476bf
Kevin Robinson <kevin.robinson.0@gmail.com>
2018-06-22 11:33:17 -0400

    Add Rollbar

--- csp-logger.js
+++ csp-logger.js
@@ -21,6 +21,20 @@ if (argv.test) {
 
 var store = require('./lib/store').init(config);
 
+// Rollbar
+var rollbar = require('rollbar');
+var shouldReportToRollbar = process.env.ROLLBAR_ACCESS_TOKEN !== undefined;
+if (shouldReportToRollbar) {
+  rollbar.init(process.env.ROLLBAR_ACCESS_TOKEN, {
+    environment: 'csp-logger',
+    deploymentKey: 'csp-logger',
+    districtKey: 'csp-logger'
+  });
+}
+
 server.listen(config.port, function (reportObject, req) {
   store.save(reportObject);
+  if (shouldReportToRollbar) {
+    rollbar.reportMessage('CSP violation', 'warning', req);
+  }
 });
